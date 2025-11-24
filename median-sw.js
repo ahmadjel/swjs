dfae9851b0250a44b317beb7a39ba1d04f5eeb27
ahmadjel <ahmad.ewaiwi.aj@gmail.com>
2025-11-24 20:02:59 +0300

    Add median UI service worker
    Implement a service worker for caching and fetching resources.

---
+++ median-sw.js
@@ -0,0 +1,42 @@
+// Median UI Service Worker v1.0
+const CACHE_NAME = 'median-ui-cache-v1';
+const urlsToCache = [
+    '/',
+    '/favicon.ico'
+];
+
+// Install event  
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Fetch event
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                return response || fetch(event.request);
+            })
+    );
+});
+
+// Activate event
+self.addEventListener('activate', function (event) {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
